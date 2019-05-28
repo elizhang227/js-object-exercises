e9113d47c28e5774c5046b785ecec699a931bc9b
elizhang227 <zhang.eli@hotmail.com>
2019-05-28 11:21:32 -0400

    person exercise and lazy greet

--- object_exercises.js
+++ object_exercises.js
@@ -17,26 +17,47 @@
 // console.log(mom);
 // console.log(daughter);
 
-function Person(name) { 
-    this.name = name; 
-    this.friends = []; 
-} 
+// function Person(name) { 
+//     this.name = name; 
+//     this.friends = []; 
+// } 
 
-Person.prototype.addFriend = function(friend) { 
-    this.friends.push(friend); 
-}; 
+// Person.prototype.addFriend = function(friend) { 
+//     this.friends.push(friend); 
+// }; 
 
-Person.prototype.createGreeting = function(other) { 
-    return 'Yo ' + other.name + '! from ' + this.name + '.'; 
-};
+// Person.prototype.createGreeting = function(other) { 
+//     return 'Yo ' + other.name + '! from ' + this.name + '.'; 
+// };
 
-Person.prototype.greet = function(other) {
-    console.log(this.createGreeting(other)); 
-};
+// Person.prototype.greet = function(other) {
+//     console.log(this.createGreeting(other)); 
+// };
 
 class Person {
     constructor (name) {
         this.name = name;
+        this.friends = [];
     }
-    addFriend 
+    addFriend(friend) {
+        this.friends.push(friend)
+        console.log('helloooooo');
+        //console.log(this.friends);
+    }
+    createGreeting(other) {
+        console.log(`Yo ${other.name}! from ${this.name}.`);
+    }
+    greet(other) {
+        this.createGreeting(other);
+    }
+    lazyGreet(other) {
+        setTimeout(()=> {this.greet(other)}, 2000);
+    }
 }
+
+test = new Person('Eli');
+test2 = new Person('RJ');
+//test.addFriend(test2);
+//test.createGreeting(test2);
+//test.greet(test2);
+test.lazyGreet(test2);
